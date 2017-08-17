e1f86f4a6fd2f88306b9285d78470077a0c8b91b
Danielle Callan <dcallan@upenn.edu>
2017-08-17 22:27:59 +0000

    timeline plot for prism
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@83952 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/Templates/Participant.pm
+++ View/lib/perl/GraphPackage/Templates/Participant.pm
@@ -30,55 +30,115 @@ sub init {
 
   my $xAxis = $self->getContXAxis();
   my $yAxis = $self->getYAxis();
-  my $eventStart = $self->getEventStart() ? $self->getEventStart() : 'EUPATH_0000644';
-  my $eventDur = $self->getEventDur() ? $self->getEventDur() : 'EUPATH_0000665';
-  my $test = $self->getTest() ? $self->getTest() : 'EUPATH_0000704';
+  my $eventStart = $self->getEventStart();
+  my $eventDur = $self->getEventDur();
+  my $status = $self->getStatus();
+  my $optStatus =  $self->getOptStatus();
 
   my $yLabel = "Weight for Height Z-score";
 
-  if ($yAxis eq 'EUPATH_0000682') {
-    $yLabel = "Head Circum for Age Z-score";
-  } elsif ($yAxis eq 'EUPATH_0000689') {
-    $yLabel = "Height for Age Z-score";
-  } elsif ($yAxis eq 'EUPATH_0000733') {
-    $yLabel = "Weight for Age Z-score";
-  } elsif ($yAxis eq 'EUPATH_0000662') {
-    $yLabel = "BMI for Age Z-score";
-  } elsif ($yAxis ne 'EUPATH_0000734') {
+  if (defined $yAxis) {
+    if ($yAxis eq 'EUPATH_0000682') {
+      $yLabel = "Head Circum for Age Z-score";
+    } elsif ($yAxis eq 'EUPATH_0000689') {
+      $yLabel = "Height for Age Z-score";
+    } elsif ($yAxis eq 'EUPATH_0000733') {
+      $yLabel = "Weight for Age Z-score";
+    } elsif ($yAxis eq 'EUPATH_0000662') {
+      $yLabel = "BMI for Age Z-score";
+    } elsif ($yAxis ne 'EUPATH_0000734') {
       warn "This option is not yet recognized. Y-axis label will need to be established in Participant.pm template.";
+    }
+  } else {
+    $yLabel = "";
   }
 
   my $xLabel = "Age in Days";
 
-  if ($xAxis ne 'EUPATH_0000644') {
+  if ($xAxis eq 'EUPATH_0000091') {
+    $xLabel = "Date of Visit";
+  } elsif ($xAxis ne 'EUPATH_0000644') {
     warn "This option is not yet recognized. X-axis label will need to be established in Participant.pm template.";
   }
 
-  my $nodeMetadata =  ({
+  my $nodeMetadata;
+  if (defined $yAxis) {
+    $nodeMetadata =  ({
                         Id => $self->getId(), 
                         contXAxis => $xAxis,  
                         yAxis => $yAxis,
                       });
-  my $nodeMetadataEvent = ({ 
+  } else {
+    $nodeMetadata =  ({
+                        Id => $self->getId(),
+                      });
+  }
+
+  my $nodeMetadataEvent;
+  if (defined $eventStart) {
+    $nodeMetadataEvent = ({ 
                             Id => $self->getId(), 
                             eventStart => $eventStart, 
                             eventDur => $eventDur,
                           });
-  my $nodeMetadataTest = ({
+  } else {
+    $nodeMetadataEvent = ({
                             Id => $self->getId(),
-                            contXAxis => $xAxis,
-                            test => $test
                           });
+  }
+
+  my $nodeMetadataStatus;
+  if (defined $status) {
+    if (defined $optStatus) {
+      $nodeMetadataStatus = ({
+                               Id => $self->getId(),
+                               contXAxis => $xAxis,
+                               status => $status,
+                               optStatus => $optStatus
+                             });
+    } else {
+      $nodeMetadataStatus = ({
+                               Id => $self->getId(),
+                               contXAxis => $xAxis,
+                               status => $status
+                             });
+    }
+  } else {
+    $nodeMetadataStatus = ({
+                               Id => $self->getId(),
+                          });
+  }
+
+  if (!defined $yAxis && !defined $eventStart && !defined $status) {
+    die "No data was provided to plot. Must provide 'yAxis', 'eventStart' or 'status' in arguments.";
+  }
+
 #TODO will eventually need for loop here mimic the one in expression.pm to allow plot parts
-  my $participantProfile = EbrcWebsiteCommon::View::GraphPackage::Util::makeNodeMetadataSet($nodeMetadata, $nodeMetadataEvent, $nodeMetadataTest);
+  my $participantProfile = EbrcWebsiteCommon::View::GraphPackage::Util::makeNodeMetadataSet($nodeMetadata, $nodeMetadataEvent, $nodeMetadataStatus);
   my $line = EbrcWebsiteCommon::View::GraphPackage::GGLinePlot::ParticipantSummary->new(@_);
   
   $line->setProfileSets($participantProfile);
   $line->setXaxisLabel($xLabel);
   $line->setYaxisLabel($yLabel);
+
+  #this will need to be improved / maybe moved somewhere else later. just trying to get it working for now.
+  if (!defined $yAxis && !defined $eventStart && defined $status) {
+    my $rAdjustString = << 'RADJUST';
+      profile.df.full$ELEMENT_NAMES = as.Date(profile.df.full$ELEMENT_NAMES, '%d-%b-%y');
+      profile.df.full$ELEMENT_NAMES_NUMERIC = NA;
+      profile.df.full = transform(profile.df.full, "COLOR"=ifelse(OPT_STATUS == 'Yes', "red", ifelse(grepl("not", STATUS), "green", "blue")));
+      profile.df.full = transform(profile.df.full, "SOLID"=ifelse((grepl("parasitemia",STATUS) | grepl("malaria",STATUS)), "solid", "hollow"));
+
+RADJUST
+  
+  $line->addAdjustProfile($rAdjustString);
+  #$line->setForceNoLines(1);
+  }
+
   $self->setGraphObjects($line);
 
   return $self;
+
 }
 
 1;
