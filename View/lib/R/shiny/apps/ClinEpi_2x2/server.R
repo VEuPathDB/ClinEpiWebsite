dd60648fa98545418fe9dba237cee103a7883bfd
Danielle Callan <dcallan@upenn.edu>
2018-02-01 16:38:59 +0000

    bug fix for non-longitudinal datasets not loading
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/branches/gems_30Jan2018@86914 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/ClinEpi_2x2/server.R
+++ View/lib/R/shiny/apps/ClinEpi_2x2/server.R
@@ -170,17 +170,19 @@ shinyServer(function(input, output, session) {
     for (col in dates) set(singleVarData, j=col, value=as.Date(singleVarData[[col]], format = "%d-%b-%y"))
 
     nums <- getNums(metadata.file)$source_id
-    if (all(longitudinal.file$columns %in% dates) | all(longitudinal.file$columns %in% nums)) {
-      numTimelines <- 1
-    } else {
-      numTimelines <- 2
-    }
-    if (numTimelines == 1) {
-      longitudinal1 <<- longitudinal.file$columns[1]
-      longitudinal2 <<- NULL
-    } else {
-      longitudinal1 <<- subset(longitudinal.file, longitudinal.file$columns %in% dates)$columns[1]
-      longitudinal2 <<- subset(longitudinal.file, longitudinal.file$columns %in% nums)$columns[1]
+    if (!nrow(longitudinal.file) == 0) {
+      if (all(longitudinal.file$columns %in% dates) | all(longitudinal.file$columns %in% nums)) {
+        numTimelines <- 1
+      } else {
+        numTimelines <- 2
+      } 
+      if (numTimelines == 1) {
+        longitudinal1 <<- longitudinal.file$columns[1]
+        longitudinal2 <<- NULL
+      } else {
+        longitudinal1 <<- subset(longitudinal.file, longitudinal.file$columns %in% dates)$columns[1]
+        longitudinal2 <<- subset(longitudinal.file, longitudinal.file$columns %in% nums)$columns[1]
+      }
     }
 
     singleVarData
