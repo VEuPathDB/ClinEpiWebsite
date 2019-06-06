cc4c5cfb5cf045ba6ba064f5f15c63d356d33b87
Danielle Callan <dcallan@upenn.edu>
2019-06-06 18:59:18 +0000

    fix ggplotly conversion bug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@98717 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/R/shiny/apps/ClinEpi_2x2/global.R
+++ View/lib/R/shiny/apps/ClinEpi_2x2/global.R
@@ -129,3 +129,57 @@ my.get_selected_slices <- function(tree, ancestry=NULL, vec=list()){
 }
 
 assignInNamespace("get_selected_slices", my.get_selected_slices, ns = "shinyTree")
+
+"%||%" <- function(x, y) {
+  if (length(x) > 0 || plotly:::is_blank(x)) x else y
+}
+
+my.get_domains <- function(nplots = 1, nrows = 1, margins = 0.01, 
+                        widths = NULL, heights = NULL) {
+  if (length(margins) == 1) margins <- rep(margins, 4)
+  if (length(margins) != 4) stop("margins must be length 1 or 4", call. = FALSE)
+  ncols <- ceiling(nplots / nrows)
+  widths <- widths %||% rep(round(1 / ncols, 2), ncols)
+  heights <- heights %||% rep(round(1 / nrows, 2), nrows)
+  if (length(widths) != ncols) {
+    stop("The length of the widths argument must be equal ",
+         "to the number of columns", call. = FALSE)
+  }
+  if (length(heights) != nrows) {
+    stop("The length of the heights argument is ", length(heights),
+         ", but the number of rows is ", nrows, call. = FALSE)
+  }
+  if (any(widths < 0) | any(heights < 0)) {
+    stop("The widths and heights arguments must contain positive values")
+  }
+  if (sum(widths) > 1 | sum(heights) > 1) {
+    stop("The sum of the widths and heights arguments must be less than 1")
+  }
+  
+  widths <- cumsum(c(0, widths))
+  heights <- cumsum(c(0, heights))
+  # 'center' these values if there is still room left 
+  widths <- widths + (1 - max(widths)) / 2
+  heights <- heights + (1 - max(heights)) / 2
+  
+  xs <- vector("list", ncols)
+  for (i in seq_len(ncols)) {
+    xs[[i]] <- c(
+      xstart = widths[i] + if (i == 1) 0 else margins[1],
+      xend = widths[i + 1] - if (i == ncols) 0 else margins[2]
+    )
+  }
+  xz <- rep_len(xs, nplots)
+  
+  ys <- vector("list", nrows)
+  for (i in seq_len(nplots)) {
+    j <- ceiling(i / ncols)
+    ys[[i]] <- c(
+      ystart = 1 - (heights[j]) - if (j == 1) 0 else margins[3],
+      yend = 1 - (heights[j + 1]) + if (j == nrows) 0 else margins[4]
+    )
+  }
+  plotly:::list2df(Map(c, xz, ys))
+}
+
+assignInNamespace("get_domains", my.get_domains, ns = "plotly")
