d2abb909106aac8b1445341a3de0df6250edccb8
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2019-10-24 00:41:53 -0400

    verbiage and style for shiny message

--- View/lib/R/shiny/functions/js/disconnectMod.js
+++ View/lib/R/shiny/functions/js/disconnectMod.js
@@ -1,5 +1,5 @@
 // check if timeout exists upon disconnect, if so offer to reload
-// if it was not a timeout, we say server error and offer link to contact us form
+// if not a timeout, a bug, we offer link to contact us
 
 var myShinyAppsVar = setInterval(myCheckForTimeout, 1000);
 var myStyleApps = document.createElement('style');
@@ -8,7 +8,19 @@ div#ss-connect-dialog {
   width: 30em;
   top: 10em;
   height: 6em;
-  border: 1px solid #9999FF;
+  border: 2px solid blue;
+  font-size: 110%;
+  background-color: white;
+}
+div#ss-connect-dialog a, div#ss-connect-dialog a:visited {
+ color: blue;
+ text-decoration: underline;
+}
+div#ss-connect-dialog label {
+  color: black;
+}
+div.modal-body {
+  display: none;
 }
 div#shiny-disconnected-overlay, div.modal-backdrop.fade.in  {
   opacity: 0;
@@ -22,16 +34,20 @@ document.head.appendChild(myStyleApps);
 function myCheckForTimeout() {
   if(document.body.contains(document.getElementById('shiny-modal-wrapper'))){
     console.log('Timeout!');
-    document.getElementById('ss-connect-dialog').style.fontSize = '140%';
-    document.getElementById('ss-reload-link').textContent = 'Inactivity timeout; click to reload the analysis.';
+    //document.getElementById('ss-connect-dialog').style.fontSize = '140%';
+    //document.getElementById('ss-reload-link').textContent = 'Timeout due to inactivity. Reload the analysis.';
+    document.querySelector('#ss-connect-dialog label').textContent = 'Timeout due to inactivity.';
+    document.getElementById('ss-reload-link').textContent = 'Reload the analysis.';
     clearInterval(myShinyAppsVar);
   } else {
       //console.log('NO Timeout!');
       if(document.body.contains(document.getElementById('ss-reload-link'))){
         clearInterval(myShinyAppsVar);
 	document.getElementById('ss-reload-link').style.display = 'none';
+        document.querySelector('#ss-connect-dialog label').textContent = 'You hit a bug: open a new analysis and select different parameters.';
         var a = document.createElement('a');
-        var linkText = document.createTextNode("Server error; please contact us with your selections.");
+        //var linkText = document.createTextNode("You hit a bug: open a new analysis and select different parameters. Please report this bug.");
+        var linkText = document.createTextNode("Please report this bug.");
         a.appendChild(linkText);
         a.target = "_blank";
 	a.href = "/a/app/contact-us";
