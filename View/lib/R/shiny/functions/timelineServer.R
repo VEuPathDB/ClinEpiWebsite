bc0a019f73aacdadd3c8873ac0043edad179a4c0
Danielle Callan <dcallan@upenn.edu>
2020-04-30 10:17:33 -0400

    fix score and dist app bugs

--- View/lib/R/shiny/functions/timelineServer.R
+++ View/lib/R/shiny/functions/timelineServer.R
@@ -19,22 +19,23 @@ validateAndDebounceTimeline <- debounce(reactive({
 timelineData <- function(mySubset, myTimeframe1, myTimeframe2, data, longitudinal1, longitudinal2, lon1Data, lon2Data){
 
   myAggKey <- aggKey()
-  if (longitudinal1 %in% names(data) & !(longitudinal1 %in% myAggKey)) {
-    myAggKey <- c(myAggKey, longitudinal1)
-  }
-  if (is.null(longitudinal2)) {
-    #TODO figure if we ever need hlonData here, cached it just in case
-    if (!is.null(lon1Data)) {
-      data <- unique(merge(data, lon1Data, by=myAggKey, all.x = TRUE))
+  if (!is.null(longitudinal1)) {
+    if (longitudinal1 %in% names(data) & !(longitudinal1 %in% myAggKey)) {
+      myAggKey <- c(myAggKey, longitudinal1)
     }
-  } else {
-    if (!is.null(lon2Data)) {
-      myLon2Data <- lon2Data
-      #for case where user selection matches second timeline variable
-      if (longitudinal2 %in% names(data)) {
-        myLon2Data <- unique(myLon2Data[, -longitudinal2, with=FALSE])
+    if (is.null(longitudinal2)) {
+      if (!is.null(lon1Data)) {
+        data <- unique(merge(data, lon1Data, by=myAggKey, all.x = TRUE))
       }
-      data <- unique(merge(data, myLon2Data, by = myAggKey, all.x = TRUE))
+    } else {
+      if (!is.null(lon2Data)) {
+        myLon2Data <- lon2Data
+        #for case where user selection matches second timeline variable
+        if (longitudinal2 %in% names(data)) {
+          myLon2Data <- unique(myLon2Data[, -longitudinal2, with=FALSE])
+        }
+        data <- unique(merge(data, myLon2Data, by = myAggKey, all.x = TRUE))
+      }
     }
   }
 
