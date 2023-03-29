7f0c597189b083ed0b51c8a68b750d7080d8271a
Dave Falke <dfalke@uga.edu>
2023-03-29 00:18:12 -0400

    Use monorepo assets and remove code that has been moved

--- Site/webapp/js/client/component-wrappers/RecordTable.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TableAsTree from '../components/TableAsTree';
-
-export default RecordTable => props => {
-  return 'tableIsTree' in props.table.properties
-    ? <TableAsTree {...props} />
-    : <RecordTable {...props} />;
-}
